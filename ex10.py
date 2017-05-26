27054019e993af60fd5d52ae494247b8b6815673
timothyseguiawork <timothyseguiawork@gmail.com>
2017-05-26 11:28:44 -0400

    Exercise 10: What Was That? New line character, Tab Character, \\ character, and block quote.

---
+++ ex10.py
@@ -0,0 +1,14 @@
+# Exercise 10: What Was That?
+# 05/26/17
+
+tabby_cat = "\tI'm tabbed in." # Tab escape character
+persian_cat = "I'm split\non a line." # Newline escape character
+backslash_cat = "I'm a \\ a \\ cat" # What kind of escape character is this?
+
+# Prints a list using both newline and tab escape characters.
+fat_cat = """
+I'll do a list:
+\t* Car food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
